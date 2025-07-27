5cb0ffb154c09345ca5d0517d0b278daa66583a7
tom1ong <tom.long@hooglee.com>
2025-07-27 11:26:26 -0700

    finish 0.1

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -5,6 +5,10 @@ import math
 # ## Task 0.1
 from typing import Callable, Iterable
 
+from typing import TypeVar
+
+A = TypeVar('A')
+
 #
 # Implementation of a prelude of elementary functions.
 
@@ -33,6 +37,71 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.1.
+
+def mul(x: float, y: float) -> float:
+    return x * y
+
+
+def id(x: A) -> A:
+    return x
+
+
+def add(x: float, y: float) -> float:
+    return x + y
+
+
+def neg(x: float) -> float:
+    return -x
+
+
+def lt(x: float, y: float) -> bool:
+    return x < y
+
+
+def eq(x: float, y: float) -> bool:
+    return x == y
+
+
+def max(x: float, y: float) -> float:
+    return math.max(x, y)
+
+
+def is_close(x: float, y: float) -> bool:
+    return math.abs(x - y) < 1e-2
+
+
+def sigmoid(x: float) -> float:
+    return 1 / (1 + math.e ** (-x))
+
+
+def relu(x: float) -> float:
+    return math.max(0, x) 
+
+
+def log(x: float) -> float:
+    return math.log(x)
+
+
+def exp(x: float) -> float:
+    return math.exp(x)
+
+
+def inv(x: float) -> float:
+    if x == 0:
+        ValueError("Cannot take reciprocal of zero.")
+    return 1 / x
+
+
+def log_back(x: float, d_out: float) -> float:
+    return inv(x) * d_out
+
+
+def inv_back(x: float, d_out: float) -> float:
+    return (-1 / x**2) * d_out
+
+
+def relu_back(x: float, d_out: float) -> float:
+    return d_out if x > 0 else 0
 
 
 # ## Task 0.3
