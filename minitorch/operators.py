bcafb9a54141decc2ed1651945ac78856d86f6b6
ilml <mytomlong@gmail.com>
2025-07-27 14:27:52 -0700

    module 1 ok

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -3,11 +3,13 @@
 import math
 
 # ## Task 0.1
-from typing import Callable, Iterable
+from typing import Callable, Iterable, Iterator
 
 from typing import TypeVar
 
 A = TypeVar('A')
+T = TypeVar('T')
+U = TypeVar('U')
 
 #
 # Implementation of a prelude of elementary functions.
@@ -63,11 +65,11 @@ def eq(x: float, y: float) -> bool:
 
 
 def max(x: float, y: float) -> float:
-    return math.max(x, y)
+    return x if x > y else y
 
 
 def is_close(x: float, y: float) -> bool:
-    return math.abs(x - y) < 1e-2
+    return abs(x - y) < 1e-2
 
 
 def sigmoid(x: float) -> float:
@@ -75,7 +77,7 @@ def sigmoid(x: float) -> float:
 
 
 def relu(x: float) -> float:
-    return math.max(0, x) 
+    return max(0, x) 
 
 
 def log(x: float) -> float:
@@ -121,3 +123,49 @@ def relu_back(x: float, d_out: float) -> float:
 
 
 # TODO: Implement for Task 0.3.
+
+
+def map(fn: Callable[[T], U], iter: Iterable[T]) -> Iterator[U]:
+    for it in iter:
+        yield fn(it)
+
+    
+def zipWith(fn: Callable[[A, T], U], iter1: Iterable[A], iter2: Iterable[T]) -> Iterator[U]:
+    for x, y in zip(iter1, iter2):
+        yield fn(x, y)
+
+
+def reduce(fn: Callable[[U, U], U], seq: Iterable[U], initial: U = None) -> U:
+    it = iter(seq)
+    
+    if initial is None:
+        try:
+            acc = next(it)
+        except StopIteration:
+            raise ValueError("reduce() of empty sequence with no initial value")
+    else:
+        acc = initial
+    
+    for item in it:
+        acc = fn(acc, item)
+    return acc
+
+
+def negList(seq: list[float]) -> list[float]:
+    return list(map(neg, seq))
+
+
+def addLists(seq1: list[float], seq2: list[float]) -> list[float]:
+    return list(zipWith(add, seq1, seq2))
+
+
+def sum(seq: list[float]) -> float:
+    if not seq:  # Handle empty list
+        return 0.0
+    return reduce(add, seq)
+
+
+def prod(seq: list[float]) -> float:
+    if not seq:  # Handle empty list
+        return 1.0
+    return reduce(mul, seq)
